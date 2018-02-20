6fc6f7a1bdaa7ed60a804ccb6b6be41e1936b928
ajn008 <ajn008@bucknell.edu>
2018-02-20 00:34:28 -0500

    db model and services done

---
+++ models/inventoryItem.model.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+var mongoosePaginate = require('mongoose-paginate')
+
+
+var InventoryItemSchema = new mongoose.Schema({
+    date: Date,
+    title: String,
+    description: String,
+    keywords: [String],
+    image: { data: Buffer, contentType: String }
+})
+
+InventoryItemSchema.plugin(mongoosePaginate)
+
+const InventoryItem = mongoose.model('InventoryItem', InventoryItemSchema)
+module.exports = InventoryItem;
---
+++ services/inventoryItem.service.js
@@ -0,0 +1,115 @@
+// get inventory item model
+var InventoryItem = require('../models/inventoryItem.model')
+
+// saving this into variable
+_this = _this
+
+
+// asynchronous function to get inventory items
+// =============================================================================
+export.getInventoryItems = async function(query, page, limit){
+
+  // setup options for mongoose paginate
+  var options = {
+    page,
+    limit
+  }
+
+
+  // try/catch for paginate promise
+  try {
+
+    var inventoryItems = await InventoryItem.paginate(query, options)
+    return inventoryItems
+
+  } catch (e) {
+    throw Error("Error Paginating Inventory Items")
+  }
+}
+// =============================================================================
+
+
+// asynchronous function to create an inventory item
+// =============================================================================
+exports.createInventoryItem = async function(inventoryItem){
+
+  // create mongoose object
+  var newInventoryItem = new InventoryItem({
+    date: new Date(),
+    title: inventoryItem.title,
+    description: inventoryItem.description,
+    keywords: inventoryItem.keywords,
+    image: inventoryItem.image
+  })
+
+
+  // try/catch for saving inventory item
+  try{
+    var savedInventoryItem = await newInventoryItem.save()
+    return savedInventoryItem
+  }catch(e){
+    throw Error("Error Creating Inventory Item")
+  }
+
+}
+// =============================================================================
+
+
+// asynchronous function to update an inventory item
+// =============================================================================
+exports.updateInventoryItem = async function(inventoryItem){
+  var id = inventoryItem.id
+
+  // try/catch retrieving inventory item to update
+  try{
+    var oldInventoryItem = await InventoryItem.findById(id)
+  }catch(e){
+    throw console.error("Error Retrieving Inventory Item")
+  }
+
+  // return false if no object
+  if(!oldInventoryItem){
+    return false
+  }
+
+  console.log(oldInventoryItem)
+
+  // update object
+  oldInventoryItem.title = inventoryItem.title
+  oldInventoryItem.description = inventoryItem.description
+  oldInventoryItem.keywords = inventoryItem.keywords
+  oldInventoryItem.image = inventoryItem.image
+
+  console.log(oldInventoryItem)
+
+  // try/catch save the changes to inventory item
+  try{
+    var savedInventoryItem = await oldInventoryItem.save()
+    return savedInventoryItem
+  }catch(e){
+    throw Error("Error Saving Changes to Inventory Item")
+  }
+
+}
+// =============================================================================
+
+
+// asynchronous function to delete an inventory item
+// =============================================================================
+exports.deleteInventoryItem = async function(id){
+
+  // try/catch delete inventory item
+  try{
+    var deleted = await InventoryItem.remove({_id: id})
+
+    if(deleted.result.n == 0){ // if 0 docs were deleted, throw error
+      throw Error("Could Not Delete Inventory Item")
+    }
+    
+    return deleted
+  }catch(e){
+    throw Error("Error Deleting Inventory Item")
+  }
+
+}
+// =============================================================================
