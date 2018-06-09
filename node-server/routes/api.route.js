2e6627b99816ba5c73fc9659013767ec47876c83
unknown <nyhusandrew@gmail.com>
2018-06-09 01:26:23 -0400

    starting over

--- node-server/routes/api.route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-var inventoryItem = require('./api/inventoryItem.route')
-
-// use routes
-router.use('/inventoryItem', inventoryItem)
-
-// export router
-module.exports = router;
